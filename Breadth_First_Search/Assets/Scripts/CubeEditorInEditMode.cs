5a08f787a2a8c7b935d32e54db97817b434daee5
Thomas Redekop <redetho@gmail.com>
2023-04-29 23:08:21 -0300

    game base

---
+++ Breadth_First_Search/Assets/Scripts/CubeEditorInEditMode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+                
+                                                          // Script for snapping the nodes / cube in the editor
+                                                          // The size of cube is currently 10 in x and z
+[ExecuteInEditMode]                                       // This script will be executed in edit mode. No need to go to play mode
+[RequireComponent(typeof(Node))]
+public class CubeEditorInEditMode : MonoBehaviour
+{
+    private Node _node;
+    private int _gridSize;                                // Grid snap units; Gets initialized from Node.cs
+
+
+
+    private void OnEnable()
+    {
+        _node = GetComponent<Node>();                     // Need to get node here else wont work
+    }
+
+
+
+    void Start()
+    {
+        _gridSize = _node.GridSize;
+    }
+
+
+
+    void Update()
+    {
+        SnapToGrid();
+    }
+
+    private void SnapToGrid()
+    {
+        // For snapping to grid
+        transform.position = new Vector3(_node.GetPos().x * _gridSize, 0f, _node.GetPos().y * _gridSize);
+    }
+}
---
+++ Breadth_First_Search/Assets/Scripts/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    
+    [SerializeField] private int _gridSize = 11;                              // Grid snap units
+    public int GridSize { get { return _gridSize; } }
+
+    public bool isExplored = false;
+    public Node isExploredFrom;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+    // Get current position; Used in CubeEditorInEditMode.cs for snapping to grid
+    public Vector2Int GetPos()
+    {
+        return new Vector2Int(Mathf.RoundToInt(transform.position.x / _gridSize), 
+                              Mathf.RoundToInt(transform.position.z / _gridSize));
+    }
+}
