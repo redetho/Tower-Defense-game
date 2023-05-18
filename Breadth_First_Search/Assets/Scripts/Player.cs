dddf24f873240df2d42dafa85b7f36a4613ed40a
Thomas Redekop <redetho@gmail.com>
2023-05-18 16:21:14 -0300

    enemy A.I., spawner do daniel implementado + cena principal + assets 3d + inimigos da júlia

--- Breadth_First_Search/Assets/Scripts/Player.cs
+++ Breadth_First_Search/Assets/Scripts/Player.cs
@@ -6,14 +6,16 @@ public class Player : MonoBehaviour
 {
     private SearchPath _searchPath;
 
-    [SerializeField] private float _movementSmoothing;
+    [SerializeField] private float _movementSpeed = 10;
     [SerializeField] private float _yOffset;
+    [SerializeField] private float _rotationSpeed = 10;
+    private int _currentPathIndex;
 
 
     private void Start()
     {
         _searchPath = FindObjectOfType<SearchPath>();
-        if(_searchPath.Path != null)        // If the player has a path to move on 
+        if (_searchPath.Path != null)    // If the player has a path to move on 
         {
             StartCoroutine(Movement(_searchPath.Path));
         }
@@ -21,12 +23,27 @@ public class Player : MonoBehaviour
 
     IEnumerator Movement(List<Node> paths)
     {
-        foreach (Node path in paths) {
+        _currentPathIndex = 0;
 
-            Vector3 pos = path.transform.position;
+        while (_currentPathIndex < paths.Count)
+        {
+            Node currentPath = paths[_currentPathIndex];
+            Vector3 targetPosition = new Vector3(currentPath.transform.position.x, _yOffset, currentPath.transform.position.z);
 
-            transform.position = new Vector3(pos.x, _yOffset, pos.z);
-            yield return new WaitForSeconds(_movementSmoothing);
+            while (transform.position != targetPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, _movementSpeed * Time.deltaTime);
+
+                // Calculate the rotation angle towards the next target position
+                Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+
+                // Rotate faster by multiplying the rotation angle with rotation speed
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+                yield return null;
+            }
+
+            _currentPathIndex++;
         }
 
         GameManager.Instance.LoadNextLevel();
